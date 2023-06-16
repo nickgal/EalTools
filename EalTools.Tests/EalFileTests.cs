791cd5e2b202fe755fdc3bee16774b730dbd8f4e
Nick Gal <nickgal@me.com>
2023-06-15 21:27:23 -0700

    Add readme

---
+++ EalTools.Tests/EalFileTests.cs
@@ -0,0 +1,26 @@
+namespace EalTools.Tests;
+
+public class EalFileTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    [TestCase("eagle-tools-logo.webp", false)]
+    [TestCase("Environments1.eal", true)]
+    [TestCase("Environments2.eal", true)]
+    [TestCase("Environments3.eal", true)]
+    [TestCase("Environments4.eal", true)]
+    [TestCase("Environments5.eal", true)]
+    [TestCase("Example.eal", true)]
+    [TestCase("Obstacles.eal", true)]
+    public void Initializes_EalFile(string filename, bool result)
+    {
+        var stream = FixtureFileLoader.LoadFileToStream("Fixtures/Eal", filename);
+        var ealFile = new EalFile(stream);
+
+        Assert.That(ealFile.Initialize(), Is.EqualTo(result));
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# EalTools
+
+<p align="center">
+<img src="EalTools.Tests\Fixtures\Eal\eagle-tools-logo.webp" width="200">
+</p>
